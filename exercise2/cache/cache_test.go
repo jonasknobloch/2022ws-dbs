cbc1b5c77d382706d53686eaa3ef016bb9ff97c0
Jonas Knobloch <jonas.knobloch@t-online.de>
2022-12-15 18:12:25 +0100

    Implement reference log

--- exercise2/cache/cache_test.go
+++ exercise2/cache/cache_test.go
@@ -5,34 +5,98 @@ import (
 	"testing"
 )
 
-func TestLRUK(t *testing.T) {
-	lruk := NewLRUK(8)
+func TestCache(t *testing.T) {
+	lru1 := NewLRUK(8, 1)
 	fifo := NewFIFO(8)
 	lifo := NewLIFO(8)
 
 	for i := 0; i < 8; i++ {
-		lruk.Put(i, i+1)
+		lru1.Put(i, i+1)
 		fifo.Put(i, i+1)
 		lifo.Put(i, i+1)
 	}
 
-	fmt.Println(Serialize(lruk))
-	fmt.Println(Serialize(fifo))
-	fmt.Println(Serialize(lifo))
+	fmt.Println("LRU1", Serialize(lru1))
+	fmt.Println("FIFO", Serialize(fifo))
+	fmt.Println("LIFO", Serialize(lifo))
 
-	lruk.Get(1)
+	lru1.Get(1)
 	fifo.Get(1)
 	lifo.Get(1)
 
-	fmt.Println(Serialize(lruk))
-	fmt.Println(Serialize(fifo))
-	fmt.Println(Serialize(lifo))
+	fmt.Println("LRU1", Serialize(lru1))
+	fmt.Println("FIFO", Serialize(fifo))
+	fmt.Println("LIFO", Serialize(lifo))
 
-	lruk.Put(9, 10)
+	lru1.Put(9, 10)
 	fifo.Put(9, 10)
 	lifo.Put(9, 10)
 
-	fmt.Println(Serialize(lruk))
-	fmt.Println(Serialize(fifo))
-	fmt.Println(Serialize(lifo))
+	fmt.Println("LRU1", Serialize(lru1))
+	fmt.Println("FIFO", Serialize(fifo))
+	fmt.Println("LIFO", Serialize(lifo))
+}
+
+func TestLRU2(t *testing.T) {
+	lru2 := NewLRUK(3, 2)
+
+	const A = 1
+	const B = 2
+	const C = 3
+	const D = 4
+
+	lru2.Put(A, 0)
+	lru2.Put(B, 0)
+	lru2.Put(C, 0)
+
+	lru2.t = 50
+	lru2.Get(C)
+	lru2.t = 52
+	lru2.Get(A)
+	lru2.t = 55
+	lru2.Get(B)
+	lru2.t = 60
+	lru2.Get(B)
+	lru2.t = 62
+	lru2.Get(A)
+	lru2.t = 65
+	lru2.Get(C)
+	lru2.t = 70
+	lru2.Get(B)
+	lru2.t = 72
+	lru2.Get(B)
+	lru2.t = 75
+	lru2.Get(C)
+	lru2.t = 76
+	lru2.Get(A)
+
+	fmt.Println(Serialize(lru2))
+
+	lru2.t = 80
+	lru2.Put(D, 0)
+
+	fmt.Println(Serialize(lru2))
+
+	for i, v := range lru2.Log() {
+		if v[0] == 0 {
+			continue
+		}
+
+		var k string
+
+		switch v[0] {
+		case 1:
+			k = "A"
+		case 2:
+			k = "B"
+		case 3:
+			k = "C"
+		case 4:
+			k = "D"
+		default:
+			k = "X"
+		}
+
+		fmt.Println(i, k)
+	}
 }
--- exercise2/cache/lruk.go
+++ exercise2/cache/lruk.go
@@ -1,14 +1,17 @@
 package cache
 
-// TODO implement (current implementation equals LRU-1)
-
 type LRUK struct {
 	data []*Entry
+	log  map[*Entry][]int
+	t    int // time
+	k    int // references
 }
 
-func NewLRUK(capacity int) *LRUK {
+func NewLRUK(capacity int, k int) *LRUK {
 	return &LRUK{
 		data: make([]*Entry, capacity),
+		log:  make(map[*Entry][]int),
+		k:    k,
 	}
 }
 
@@ -31,6 +34,18 @@ func (c *LRUK) Get(key int) *Entry {
 
 	c.data = append(append(c.data[:p], c.data[p+1:]...), e)
 
+	if c.log[e] == nil {
+		c.log[e] = make([]int, c.k)
+	}
+
+	if len(c.log[e]) < c.k {
+		c.log[e] = append(c.log[e], c.t)
+	} else {
+		c.log[e] = append(c.log[e][1:], c.t)
+	}
+
+	c.t++
+
 	return e
 }
 
@@ -47,9 +62,39 @@ func (c *LRUK) Put(key, value int) {
 		}
 	}
 
-	c.data[len(c.data)-1] = e
+	var p int // position
+	var m int // max delta
+
+	p = len(c.data) - 1
+
+	for i, v := range c.data {
+		if len(c.log[v]) < c.k {
+			continue
+		}
+
+		d := c.t - c.log[v][0]
+
+		if d > m {
+			m = d
+			p = i
+		}
+	}
+
+	c.data[p] = e
 }
 
 func (c *LRUK) Data() []*Entry {
 	return c.data
+}
+
+func (c *LRUK) Log() [][2]int {
+	log := make([][2]int, c.t)
+
+	for e, t := range c.log {
+		for _, v := range t {
+			log[v] = [2]int{e.Key, e.Value}
+		}
+	}
+
+	return log
 }
